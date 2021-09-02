d5fc88e9fbdecd7c677597698304ea1ecedd07f3
wuare <wuareb@gmail.com>
2021-09-02 15:42:25 +0800

    Bump version

---
+++ src/main/java/io/github/wuare/ot/OtherClientTest.java
@@ -0,0 +1,31 @@
+package io.github.wuare.ot;
+
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class OtherClientTest {
+
+    public static void main(String[] args) throws Exception {
+        Socket s = new Socket("localhost", 8088);
+
+        OutputStream outputStream = s.getOutputStream();
+        byte[] buf = new byte[4096];
+        int i = 0;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+        for (;;) {
+            i++;
+            byteBuffer.putInt(i);
+            byte[] array = byteBuffer.array();
+            byteBuffer.clear();
+            buf[0] = array[0];
+            buf[1] = array[1];
+            buf[2] = array[2];
+            buf[3] = array[3];
+            outputStream.write(buf);
+            outputStream.flush();
+            Thread.sleep(100);
+            System.out.println("send content: " + i);
+        }
+    }
+}
---
+++ src/main/java/io/github/wuare/ot/OtherServerTest.java
@@ -0,0 +1,46 @@
+package io.github.wuare.ot;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class OtherServerTest {
+
+    private final int port;
+
+    public OtherServerTest(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        ServerSocket ss = new ServerSocket(port);
+        Socket socket = ss.accept();
+        InputStream inputStream = socket.getInputStream();
+        byte[] buf = new byte[4096];
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+        for (;;) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            int read = inputStream.read(buf);
+            System.out.println("read buf length: " + read);
+            byteBuffer.put(buf[0]);
+            byteBuffer.put(buf[1]);
+            byteBuffer.put(buf[2]);
+            byteBuffer.put(buf[3]);
+            byteBuffer.flip();
+            int anInt = byteBuffer.getInt();
+            byteBuffer.clear();
+            System.out.println("read content: " + anInt);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new OtherServerTest(8088).start();
+    }
+}
