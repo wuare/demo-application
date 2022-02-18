d84d37495060126f9c4dd8b9585503c8b7d93df3
wuare <wuareb@gmail.com>
2022-02-18 10:55:30 +0800

    Update code

--- src/main/java/io/github/wuare/hl/controller/LangController.java
+++ src/main/java/io/github/wuare/hl/controller/LangController.java
@@ -5,7 +5,6 @@ import io.github.wuare.hl.anno.PostMapping;
 import top.wuare.http.proto.HttpRequest;
 import top.wuare.http.proto.HttpResponse;
 import top.wuare.lang.interpreter.Interpreter;
-import top.wuare.lang.type.ReturnValue;
 
 import java.math.BigDecimal;
 
@@ -16,25 +15,20 @@ public class LangController {
     public void eval(HttpRequest request, HttpResponse response) {
         String code = request.getBody();
         Interpreter interpreter = new Interpreter(code);
-        Object eval = interpreter.eval();
+        Object val = interpreter.eval();
         String consoleText = interpreter.getConsole().toString() + "<br/>";
-        if (eval instanceof ReturnValue) {
-            Object val = ((ReturnValue) eval).getVal();
-            if (val == null) {
-                response.setBody(consoleText + "null");
-                return;
-            }
-            if (val instanceof BigDecimal) {
-                response.setBody(consoleText + ((BigDecimal) val).toPlainString());
-                return;
-            }
-            if (val instanceof Boolean) {
-                response.setBody(consoleText + ((Boolean) val).toString());
-                return;
-            }
-            response.setBody(consoleText + val.toString());
+        if (val == null) {
+            response.setBody(consoleText + "null");
             return;
         }
-        response.setBody(consoleText + "执行完成，无返回值");
+        if (val instanceof BigDecimal) {
+            response.setBody(consoleText + ((BigDecimal) val).toPlainString());
+            return;
+        }
+        if (val instanceof Boolean) {
+            response.setBody(consoleText + ((Boolean) val).toString());
+            return;
+        }
+        response.setBody(consoleText + val.toString());
     }
 }
