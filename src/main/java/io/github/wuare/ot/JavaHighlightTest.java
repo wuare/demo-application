1581b254b6536e473694b3a17d24965cab2a27a3
wuare <wuareb@gmail.com>
2022-02-24 10:47:56 +0800

    Update json.html

--- src/main/java/io/github/wuare/ot/JavaHighlightTest.java
+++ src/main/java/io/github/wuare/ot/JavaHighlightTest.java
@@ -1,32 +1,19 @@
 package io.github.wuare.ot;
 
-import java.util.logging.Logger;
-
 /**
  * @author wuare
  */
 public class JavaHighlightTest {
 
-    private static final Logger logger = Logger.getLogger(JavaHighlightTest.class.getName());
-
-    public int num(int a, int b) {
-        // add two operand and return result
-        return a + b;
-    }
-
-    public int number() {
-        return 1000;
-    }
-
-    public String str() {
-        return "高亮测试";
-    }
+    private static final int a = 1000;
 
     @Deprecated
-    public void anno() {
-    }
-
-    public void test() {
-        logger.info("test...");
+    public int test(int a, int b) {
+        // 字符串
+        String s = "字符串测试";
+        // 数字
+        double d = 1.00;
+        // add two operand and return result
+        return a + b;
     }
 }
