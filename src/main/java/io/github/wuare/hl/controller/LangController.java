e8df924fbd2071a17b7c1c699716f952735c7ddd
wuare <wuareb@gmail.com>
2022-03-03 17:58:34 +0800

    Support preview

--- src/main/java/io/github/wuare/hl/controller/LangController.java
+++ src/main/java/io/github/wuare/hl/controller/LangController.java
@@ -5,9 +5,12 @@ import io.github.wuare.hl.anno.PostMapping;
 import top.wuare.http.proto.HttpRequest;
 import top.wuare.http.proto.HttpResponse;
 import top.wuare.lang.interpreter.Interpreter;
+import top.wuareb.highlight.gen.html.wa.WaGen;
 
 @Controller
 public class LangController {
+
+    private final WaGen waGen = new WaGen();
 
     @PostMapping("/lang/eval")
     public void eval(HttpRequest request, HttpResponse response) {
@@ -16,5 +19,14 @@ public class LangController {
         Object val = interpreter.eval();
         String consoleText = interpreter.getConsole().toString() + "<br/>";
         response.setBody(consoleText);
+    }
+
+    @PostMapping("/lang/hl")
+    public void wa(HttpRequest request, HttpResponse response) {
+        String text = request.getBody();
+        response.setBody("");
+        if (text != null) {
+            response.setBody( waGen.gen(text));
+        }
     }
 }
