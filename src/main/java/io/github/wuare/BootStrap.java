372fd962af7598ad9589295c4927525b01da956e
wuare <wuareb@gmail.com>
2022-03-08 11:37:16 +0800

    Rename log config file

--- src/main/java/io/github/wuare/BootStrap.java
+++ src/main/java/io/github/wuare/BootStrap.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class BootStrap {
     public static void main(String[] args) throws IOException {
+        // LogManager.getLogManager().readConfiguration(BootStrap.class.getClassLoader().getResourceAsStream("log.properties"));
         ServerContainer.instance.start();
     }
 }
